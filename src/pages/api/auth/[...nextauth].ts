2cb8d1f8125522563088fcd5a8ae81a6e6f39de0
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-09-24 16:26:39 +0200

    custom signin page

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -25,6 +25,9 @@ export const authOptions: NextAuthOptions = {
     }),
     // ...add more providers here
   ],
+  pages: {
+    signIn: "/signin",
+  },
 };
 
 export default NextAuth(authOptions);
---
+++ src/pages/signin.tsx
@@ -0,0 +1,46 @@
+import { GetServerSideProps } from "next";
+import { getProviders, getSession, signIn } from "next-auth/react";
+
+function SignIn(props: {
+  providers: Awaited<ReturnType<typeof getProviders>>;
+}) {
+  if (!props.providers) return <div>No auth providers found</div>;
+
+  return (
+    <div className="flex min-h-screen w-screen flex-col items-center justify-center gap-4 bg-slate-800">
+      <h1 className="text-center text-4xl text-white">
+        Welcome to the custom login page
+      </h1>
+      {Object.values(props.providers).map((provider) => (
+        <button
+          key={provider.id}
+          className="bg-white hover:bg-slate-200 p-2"
+          onClick={() => signIn(provider.id)}
+        >
+          Log in with {provider.name}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default SignIn;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  const providers = await getProviders();
+  return {
+    props: {
+      providers,
+    },
+  };
+};
