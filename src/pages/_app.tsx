b93488b35c69edcdf22976081f5bd27eb65501a8
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-10-12 17:48:48 +0200

    extract AppLayout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,26 +7,34 @@ import { trpc } from "../utils/trpc";
 import Header from "../modules/header/Header";
 import { useRouter } from "next/router";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactNode } from "react";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
-  const router = useRouter();
-  const fullscreen = ["/signin"].indexOf(router.pathname) !== -1;
-
+const MyApp: AppType<{
+  session: Session | null;
+}> = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <SessionProvider session={session}>
-      <div className="flex min-h-screen w-screen flex-col items-center bg-slate-800">
-        {!fullscreen && <Header />}
-        <main className="container flex h-full w-screen flex-1 flex-col gap-2">
-          <Component {...pageProps} />
-        </main>
-      </div>
-      {/* TODO: only show this in dev */}
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+      {/* only shows up in dev */}
       <ReactQueryDevtools />
     </SessionProvider>
   );
 };
 
 export default trpc.withTRPC(MyApp);
+
+function AppLayout(props: { children: ReactNode }) {
+  const router = useRouter();
+  const fullscreen = ["/signin"].indexOf(router.pathname) !== -1;
+
+  return (
+    <div className="flex min-h-screen w-screen flex-col items-center bg-slate-800">
+      {!fullscreen && <Header />}
+      <main className="container flex h-full w-screen flex-1 flex-col gap-2">
+        {props.children}
+      </main>
+    </div>
+  );
+}
